97eea7bf0ea58b7701ca4c29371180d8a8f6b031
Mykola Humeniak <m.gumenyak@gmail.com>
2017-03-05 18:02:04 +0000

    AutoPlants - lcd added

--- projects/AutoPlants/AutoPlants.ino
+++ projects/AutoPlants/AutoPlants.ino
@@ -1,3 +1,26 @@
+#include <Boards.h>
+#include <Firmata.h>
+#include <LiquidCrystal_I2C.h>
+
+#define BACKLIGHT_PIN     13
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
+
+// Creat a set of new characters
+const uint8_t charBitmap[][8] = {
+   { 0xc, 0x12, 0x12, 0xc, 0, 0, 0, 0 },
+   { 0x6, 0x9, 0x9, 0x6, 0, 0, 0, 0 },
+   { 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0, 0x0 },
+   { 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0, 0x0 },
+   { 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0, 0x0 },
+   { 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0, 0x0 },
+   { 0x0, 0x0, 0x0, 0x6, 0x9, 0x9, 0x6, 0x0 },
+   { 0x0, 0x0, 0x0, 0xc, 0x12, 0x12, 0xc, 0x0 }
+   
+};
+
+
+
 const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
 //const int analogOutPin = 9; // Analog output pin that the LED is attached to
 
@@ -22,6 +45,19 @@ void setup() {
   pinMode(switchPumpPin, OUTPUT);
   digitalWrite(releyPin, HIGH);
   digitalWrite(switchPumpPin, HIGH);
+
+  int charBitmapSize = (sizeof(charBitmap ) / sizeof (charBitmap[0]));
+
+  // Switch on the backlight
+  pinMode ( BACKLIGHT_PIN, OUTPUT );
+  digitalWrite ( BACKLIGHT_PIN, HIGH );
+  
+  lcd.begin(16,2);               // initialize the lcd 
+
+  for ( int i = 0; i < charBitmapSize; i++ )
+  {
+     lcd.createChar ( i, (uint8_t *)charBitmap[i] );
+  }
 }
 
 void delaySec( int iSec )
@@ -42,8 +78,14 @@ int averLight( int iIterNum, int iSecDelay )
       averageLight += (analogRead(analogInPin) / iIterNum );
       delaySec(iSecDelay);
     }
-    Serial.print("average light - ");
-    Serial.println(averageLight);
+//    lcd.home();
+//    lcd.print("Average light - ");
+//    lcd.setCursor ( 0, 1 );
+//    lcd.print("value - "); 
+//    lcd.print(averageLight);
+//    delay(3000);
+//    Serial.print("average light - ");
+//    Serial.println(averageLight);
     return averageLight;
 }
 
@@ -66,35 +108,77 @@ void watering(int iTimeSec)
 }
 
 void loop() {
-  // read the analog in value:
-
+  lcd.backlight();
+  lcd.display();
+  
   amountOfLight = averLight(15, 5);
+  digitalWrite ( switchSensorPin, HIGH );
+  
   if ( !isLightSwitched && (amountOfLight < 150) )
   {
-    Serial.print("switch the light on ");
-    Serial.println(amountOfLight);
+    lcd.clear();
+    lcd.home();
+    lcd.print("switch on");
+    delay(3000);    
+//    Serial.print("switch the light on ");
+//    Serial.println(amountOfLight);
+
     switchTheLight( 1 );
     watering(30);
     newThreshold = averLight(15, 5);
-    Serial.print("newThreshold ");
-    Serial.println(newThreshold);
+    
+    lcd.clear();
+    lcd.home();
+    lcd.print("Threshold ");
+    lcd.setCursor ( 0, 1 );
+    lcd.print(newThreshold);
+    delay(3000);
+    
+//    Serial.print("newThreshold ");
+//    Serial.println(newThreshold);
   }
   else if ( isLightSwitched && (amountOfLight > (newThreshold + 100)) )   
   {
-    delaySec(60 * 60);
+    for (int i = 0; i < 12; ++i)
+    {
+      lcd.clear();
+      lcd.home();
+      lcd.print("to switch off");
+      lcd.setCursor ( 0, 1 );
+      lcd.print((12 - i) * 5);
+      lcd.print(" min");
+      delaySec(5 * 60);
+    }
     amountOfLight = averLight(15, 5);
 //    if ( amountOfLight > 900 )
 //    {
-      Serial.print("switch the light off ");
-      Serial.println(amountOfLight);
+//      Serial.print("switch the light off ");
+//      Serial.println(amountOfLight);
       switchTheLight( 0 );
 //    }
   }
-  Serial.print("loop - "); 
-  Serial.println(amountOfLight);
+//  Serial.print("loop - "); 
+//  Serial.println(amountOfLight);
   digitalWrite(switchSensorPin, LOW);
   timeFromWatering += 1;
-  Serial.print("TIME FROM WATERING "); 
-  Serial.println(timeFromWatering);  
+  
+  lcd.clear();
+  lcd.home();
+  lcd.print("FROM WATERING ");
+  lcd.setCursor ( 0, 1 );
+  lcd.print(timeFromWatering);
+  delay(3000);
+  lcd.clear();
+  lcd.home();
+  lcd.print("Light - ");
+  lcd.print(amountOfLight);
+  lcd.setCursor ( 0, 1 );
+  lcd.print("Threshold - ");
+  lcd.print(newThreshold);
+  delay(5000);
+  lcd.noDisplay();
+  lcd.noBacklight();
+//  Serial.print("TIME FROM WATERING "); 
+//  Serial.println(timeFromWatering);  
   delaySec(60);
 }
